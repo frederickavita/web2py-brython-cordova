178dc511c4a3f9b424e64fd5cd16d460d262afe8
frederic <amihaconsulting@gmail.com>
2018-02-13 19:23:56 +0100

    documentation and annontation login.py files

--- web2py/applications/email/views/default/login.py
+++ web2py/applications/email/views/default/login.py
@@ -1,19 +1,40 @@
 from browser import document as doc
 from browser.html import DIV, BUTTON, INPUT, H1,BR
 
-
+#select container id
 container = doc['container']
 
 def login(ev):
+    """event login hide  welcome div
+    show login div 
+    
+    Arguments:
+        if but_login id is clicked
+    """
     doc['welcome'].style.display = "none"
     doc['login'].style.display = "block"
 
 def register(ev):
+    """ event register  hide welcome div
+        show register div
+    
+    Arguments:
+        if but_register id is clicked
+    """
+
     doc['welcome'].style.display = "none"
     doc['register'].style.display = "block"
 
 
 def back(ev):
+    """back event show welcome id  and hide
+       div id register or div id login
+    Arguments:
+        if ev.target.id == "back_login
+            hide login id div
+        elif ev.target.id == "back_register
+            hide register id div    
+    """
     if ev.target.id == "back_login":
         doc['login'].style.display = "none"
         doc['welcome'].style.display = "block"
@@ -22,17 +43,18 @@ def back(ev):
         doc['welcome'].style.display = "block"
 
 
-# page d'acceuil avec les deux bouttons login et register
+# welcome page with two buttons  login and register
 div1 = DIV(id="welcome")
 button1 = BUTTON("Register", id="but_register")
 button2 = BUTTON("Login", id="but_login")
 div1 <= button1 + button2
 container <= div1
-# event pour boutton show login et show register
+# event button for show login div and show register div 
 doc['but_login'].bind('click', login)
 doc['but_register'].bind('click', register)
-# page cacher de login
 
+
+#login page
 div2 = DIV(id="login", Class="w3-center")
 title1 = H1("Login in")
 input1 = INPUT(placeholder="Your email")
@@ -44,7 +66,7 @@ div2 <= title1 + input1 + input2 + BR() + \
 container <= div2
 doc['back_login'].bind('click', back)
 
-# page cacher de register
+# register page
 div3 = DIV(id="register", Class="w3-center")
 title2 = H1("Register")
 input3 = INPUT(placeholder="Your email")
